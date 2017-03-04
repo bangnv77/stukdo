91fed1c918a0f3c5211756f8eba520ba1c3f7c73
Bang Nguyen <bangnv77@gmail.com>
2017-03-04 16:50:41 -0500

    add link detection to current version of auto_html, not working as expected

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,24 @@
 class Task < ApplicationRecord
   belongs_to :user
 
-  auto_html_for :content do
-    html_escape
-    image
-    youtube(:width => "100%", :height => 250, :autoplay => false)
-    link :target => "_blank", :rel => "nofollow"
-    simple_format
+  content = AutoHtml::Pipeline.new(
+    AutoHtml::HtmlEscape.new,
+    AutoHtml::Markdown.new,
+    AutoHtml::Image,
+    AutoHtml::Link,
+    AutoHtml::SimpleFormat
+  )
+
+  def text=(t)
+    super(t)
+    self[:text_html] = content.call(t)
   end
+
+  # auto_html_for :content do
+  #   html_escape
+  #   image
+  #   youtube(:width => "100%", :height => 250, :autoplay => false)
+  #   link :target => "_blank", :rel => "nofollow"
+  #   simple_format
+  # end
 end
